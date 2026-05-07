651299ecf1d841f5de3861993ba389750c811d5e
Hameed Mulani <hameedmulani15@gmail.com>
2026-05-07 09:21:50 +0530

    Exp_8_1 added
    Implemented a stack data structure using a linked list with push, pop, peek, and display operations.

---
+++ stackUsingLL.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+using namespace std;
+
+// Node structure
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int value) {
+        data = value;
+        next = NULL;
+    }
+};
+
+// Stack using Linked List
+class Stack {
+private:
+    Node* top;
+
+public:
+    Stack() {
+        top = NULL;
+    }
+
+    // Push operation
+    void push(int value) {
+        Node* newNode = new Node(value);
+
+        newNode->next = top;
+        top = newNode;
+
+        cout << value << " pushed into stack\n";
+    }
+
+    // Pop operation
+    void pop() {
+        if (top == NULL) {
+            cout << "Stack Underflow\n";
+            return;
+        }
+
+        Node* temp = top;
+        cout << top->data << " popped from stack\n";
+
+        top = top->next;
+        delete temp;
+    }
+
+    // Peek operation
+    void peek() {
+        if (top == NULL) {
+            cout << "Stack is empty\n";
+            return;
+        }
+
+        cout << "Top element: " << top->data << endl;
+    }
+
+    // Display stack
+    void display() {
+        if (top == NULL) {
+            cout << "Stack is empty\n";
+            return;
+        }
+
+        Node* temp = top;
+
+        cout << "Stack elements:\n";
+
+        while (temp != NULL) {
+            cout << temp->data << endl;
+            temp = temp->next;
+        }
+    }
+};
+
+// Main function
+int main() {
+    Stack s;
+
+    s.push(10);
+    s.push(20);
+    s.push(30);
+
+    s.display();
+
+    s.peek();
+
+    s.pop();
+
+    s.display();
+
+    return 0;
+}
